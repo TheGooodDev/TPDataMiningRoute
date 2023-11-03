9aca96680d5560483a311763328bf2c574e5e4d9
dyzzorka <83311421+dyzzorka@users.noreply.github.com>
2023-11-03 09:57:32 +0100

    dataset

--- concatDataSet.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-part1 = pd.read_csv('Dataset/accidents-19-22_P1.csv', low_memory=False)
-part2 = pd.read_csv('Dataset/accidents-19-22_P2.csv', low_memory=False)
-part3 = pd.read_csv('Dataset/accidents-19-22_P3.csv', low_memory=False)
-merged_df = pd.concat([part1, part2, part3])
-merged_df.to_csv('accidents-19-22.csv', index=False)
